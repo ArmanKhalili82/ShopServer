fff525115c9f28820eba22b8a17253020ede9bb1
ArmanKhalili82 <hykhalili@yahoo.com>
2023-10-01 12:45:06 +0200

    Lessons

---
+++ ShopServer/Controllers/LessonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopServer.Controllers
+{
+    public class LessonController : Controller
+    {
+        private static List<Lesson> Lessons = new List<Lesson>()
+        {
+            new Lesson()
+            {
+                Price = 100
+            }
+        };
+
+        [HttpGet(Name = "GetLesson")]
+        public List<Lesson> Get()
+        {
+            return Lessons;
+        }
+
+        [HttpGet]
+        public List<Lesson> Get(int id)
+        {
+            return Lessons;
+        }
+    }
+}
---
+++ ShopServer/Lesson.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopServer
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int TimeRange { get; set; }
+        public string Teacher { get; set; }
+    }
+}
