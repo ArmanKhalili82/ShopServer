1630aa779a114a7ee9c31feb2acc81e2cb97c414
ArmanKhalili82 <hykhalili@yahoo.com>
2023-10-27 23:40:34 +0200

    dependency

--- ShopServer/Controllers/LessonController.cs
+++ ShopServer/Controllers/LessonController.cs
@@ -7,45 +7,46 @@ namespace ShopServer.Controllers
 
     public class LessonController : Controller
     {
-        
+
+        private ILessonService _LessonService;
+        public LessonController(ILessonService lessonService)
+        {
+            _LessonService = lessonService;
+        }
+
 
         [HttpGet("GetAll")]
         public IActionResult GetAll()
         {
-            private ILessonService _LessonService;
-            public LessonController(ILessonService lessonService)
-        {
-            _LessonService = lessonService;
-        }
+            _LessonService.GetAll();
             return Ok(SubjectLessons);
         }
 
         [HttpGet("GetById/{id}")]
         public IActionResult GetById(int id)
         {
-            var data = SubjectLessons.Where(l => l.Id == id).FirstOrDefault();
+            var data = _LessonService.GetById(id);
             return Ok(data);
         }
 
         [HttpPut("Update")]
         public IActionResult Update([FromBody]Lesson model)
         {
-            
-
+            _LessonService.Update(model);
             return Ok(data);
         }
 
         [HttpPost("Create")]
         public IActionResult Create([FromBody]Lesson model)
         {
-            
+            _LessonService.Create(model);
             return Ok();
         }
 
         [HttpDelete("Delete/{id}/force")]
         public IActionResult Delete(int id)
         {
-            var data = SubjectLessons.Where(l => l.Id == id).FirstOrDefault();
+            var data = _LessonService.Delete(id);
             SubjectLessons.Remove(data);
             return Ok();
         }
